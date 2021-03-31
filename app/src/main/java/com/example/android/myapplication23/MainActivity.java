54502221eba3faef944414d57c84785bb8a2c589
Anubhuti Shruti <shrutianubhuti@gmail.com>
2021-03-31 15:54:31 +0530

    First version

--- app/src/main/java/com/example/android/myapplication23/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication23/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.android.myapplication23;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +42,10 @@ public class MainActivity extends Activity {
     byte buffer[];
     int bufferPosition;
     boolean stopThread;
+    private final int REQUEST_READ_PHONE_STATE=1;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    EditText edtSmsNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +56,24 @@ public class MainActivity extends Activity {
         stopButton = (Button) findViewById(R.id.buttonStop);
         editText = (EditText) findViewById(R.id.editText);
         textView = (TextView) findViewById(R.id.textView);
+        Button sendB = (Button) findViewById(R.id.send_btn);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        int permissionCheck2 = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
+            // Toast.makeText(this, "W?", Toast.LENGTH_SHORT);
+        }
+        else {// Toast.makeText(this, "!!!", Toast.LENGTH_SHORT);
+            sendB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    sendMessage("1");
+                }
+            });
+        }
+
         setUiEnabled(false);
 
     }
@@ -159,12 +186,14 @@ public class MainActivity extends Activity {
                             byte[] rawBytes = new byte[byteCount];
                             inputStream.read(rawBytes);
                             final String string=new String(rawBytes,"UTF-8");
-                            handler.post(new Runnable() {
+                            handler.postDelayed(new Runnable() {
+                                @Override
                                 public void run()
                                 {
                                     textView.append(string);
+                                    sendMessage(textView.getText().toString());
                                 }
-                            });
+                            }, 5000);
 
                         }
                     }
@@ -203,5 +232,27 @@ public class MainActivity extends Activity {
 
     public void onClickClear(View view) {
         textView.setText("");
+    }
+
+    public void sendMessage(String string) {
+
+        String last = string.substring(string.length() - 1);
+        String secondlast = "0";
+        if(string.length() > 1)
+            secondlast = string.substring(string.length()-2);
+
+        if (last.equals("1") && secondlast.equals("0")) {
+            textView.setText("1");
+            try {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(editText.getText().toString(), null /*"\"+919934029007\",145"*/, "It worked!! btw Bhaskar is in Danger :P", null, null);
+                Toast.makeText(getApplicationContext(), "Your Message Sent",
+                        Toast.LENGTH_LONG).show();
+            } catch (Exception ex) {
+                Toast.makeText(getApplicationContext(), ex.getMessage().toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
 }
